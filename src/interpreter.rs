210286f71258526d7e631b093002bd50b92608f9
WurmWillem <84849889+WurmWillem@users.noreply.github.com>
2025-01-02 19:35:31 +0100

    refactored evaluate_expr into functions

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,6 +1,9 @@
 use core::panic;
 
-use crate::{crash, environment::Env, expr::Expr, stmt::Stmt, token_type::TokenType, value::Value};
+use crate::{
+    crash, environment::Env, expr::Expr, stmt::Stmt, token::Token, token_type::TokenType,
+    value::Value,
+};
 
 pub struct Interpreter {
     env: Env,
@@ -112,154 +115,161 @@ impl Interpreter {
         match expr {
             Expr::Lit(lit) => Value::from_lit(&lit),
             Expr::Grouping(expr) => self.evaluate_expr(expr),
-            Expr::Unary(token, expr) => {
-                let right = self.evaluate_expr(expr);
+            Expr::Unary(token, expr) => self.evaluate_unary(token, expr),
+            Expr::Binary(left, op, right) => self.evaluate_binary(left, op, right),
+            Expr::Var(token) => self.evaluate_var(token),
+            Expr::Assign(name, expr) => self.evaluate_assign(name, expr),
+            Expr::Logic(left, op, right) => self.evaluate_logic(left, op, right),
+        }
+    }
 
-                match token.kind {
-                    TokenType::Minus => match right {
-                        Value::Num(num) => Value::Num(-num),
-                        _ => crash(
-                            token.line,
-                            "Min kan alleen worden gebruikt voor nummers, kaaskop",
-                        ),
-                    },
-                    TokenType::Bang => match right.is_true() {
-                        Some(bool) => Value::from_bool(!bool),
-                        None => crash(
-                            token.line,
-                            "Uitroepteken kan alleen worden gebruikt op waarheidswaardes, kaaskop",
-                        ),
-                    },
-                    _ => panic!("Unreachable."),
-                }
-            }
-            Expr::Binary(left, op, right) => {
-                let left = self.evaluate_expr(left);
-                let right = self.evaluate_expr(right);
+    fn evaluate_unary(&mut self, token: &Token, expr: &Box<Expr>) -> Value {
+        let right = self.evaluate_expr(expr);
 
-                macro_rules! apply_arith_to_nums {
-                    ($type: ident, $op: tt) => {
-                        if let (Value::Num(num1), Value::Num(num2)) = (left, right) {
-                            Value::Num(num1 $op num2)
-                        } else {
-                            crash(op.line, concat!(stringify!($op), " kan alleen worden gebruikt op nummers, kaaskop"))
-                        }
-                    };
-                }
-
-                macro_rules! apply_logic_to_nums {
-                    ($type: ident, $op: tt) => {
-                        if let (Value::Num(num1), Value::Num(num2)) = (left, right) {
-                            Value::from_bool(num1 $op num2)
-                        } else {
-                            crash(op.line, concat!(stringify!($op), " kan alleen worden gebruikt op nummers, kaaskop"));
-                        }
-                    };
-                }
-
-                match op.kind {
-                    TokenType::Plus => match (left, right) {
-                        (Value::Num(num), Value::Str(str)) => {
-                            return Value::Str(format!("{}{}", num, str))
-                        }
-
-                        (Value::Str(str), Value::Num(num)) => {
-                            return Value::Str(format!("{}{}", str, num))
-                        }
-                        (Value::Num(num1), Value::Num(num2)) => return Value::Num(num1 + num2),
-                        (Value::Str(str1), Value::Str(str2)) => {
-                            return Value::Str(format!("{}{}", str1, str2))
-                        }
-                        _ => crash(
-                            op.line,
-                            "Plus kan alleen worden gebruikt op nummers en strings, kaaskop.",
-                        ),
-                    },
-                    TokenType::Minus => apply_arith_to_nums!(Minus, -),
-                    TokenType::Star => apply_arith_to_nums!(Star, *),
-                    TokenType::Slash => apply_arith_to_nums!(Slash, /),
-
-                    TokenType::Caret => match (left, right) {
-                        (Value::Num(num1), Value::Num(num2)) => return Value::Num(num1.powf(num2)),
-                        _ => crash(
-                            op.line,
-                            "Caret kan alleen worden gebruikt op nummers, kaaskop.",
-                        ),
-                    },
-
-                    TokenType::Greater => apply_logic_to_nums!(Greater, >),
-                    TokenType::GreaterEqual => apply_logic_to_nums!(GreaterEqual, >=),
-                    TokenType::Less => apply_logic_to_nums!(Less, <),
-                    TokenType::LessEqual => apply_logic_to_nums!(LessEqaul, <=),
-                    TokenType::Equal => apply_logic_to_nums!(Equal, ==),
-                    TokenType::EqualEqual => Value::from_bool(Value::is_equal(&left, &right)),
-                    TokenType::BangEqual => Value::from_bool(!Value::is_equal(&left, &right)),
-                    _ => panic!("Unreachable."),
-                }
-            }
-
-            // get value from variable name out of hashmap
-            Expr::Var(token) => match self.env.get_value(&token) {
-                Some(value) => value,
+        match token.kind {
+            TokenType::Minus => match right {
+                Value::Num(num) => Value::Num(-num),
+                _ => crash(
+                    token.line,
+                    "Min kan alleen worden gebruikt voor nummers, kaaskop",
+                ),
+            },
+            TokenType::Bang => match right.is_true() {
+                Some(bool) => Value::from_bool(!bool),
                 None => crash(
                     token.line,
-                    &format!("{} is een onbekende variabele.", token.lexeme),
+                    "Uitroepteken kan alleen worden gebruikt op waarheidswaardes, kaaskop",
+                ),
+            },
+            _ => panic!("Unreachable."),
+        }
+    }
+
+    fn evaluate_binary(&mut self, left: &Box<Expr>, op: &Token, right: &Box<Expr>) -> Value {
+        let left = self.evaluate_expr(left);
+        let right = self.evaluate_expr(right);
+
+        macro_rules! apply_arith_to_nums {
+            ($type: ident, $op: tt) => {
+                if let (Value::Num(num1), Value::Num(num2)) = (left, right) {
+                    Value::Num(num1 $op num2)
+                } else {
+                    crash(op.line, concat!(stringify!($op), " kan alleen worden gebruikt op nummers, kaaskop"))
+                }
+            };
+        }
+
+        macro_rules! apply_logic_to_nums {
+            ($type: ident, $op: tt) => {
+                if let (Value::Num(num1), Value::Num(num2)) = (left, right) {
+                    Value::from_bool(num1 $op num2)
+                } else {
+                    crash(op.line, concat!(stringify!($op), " kan alleen worden gebruikt op nummers, kaaskop"));
+                }
+            };
+        }
+
+        match op.kind {
+            TokenType::Plus => match (left, right) {
+                (Value::Num(num), Value::Str(str)) => return Value::Str(format!("{}{}", num, str)),
+                (Value::Str(str), Value::Num(num)) => return Value::Str(format!("{}{}", str, num)),
+                (Value::Num(num1), Value::Num(num2)) => return Value::Num(num1 + num2),
+                (Value::Str(str1), Value::Str(str2)) => {
+                    return Value::Str(format!("{}{}", str1, str2))
+                }
+
+                _ => crash(
+                    op.line,
+                    "Plus kan alleen worden gebruikt op nummers en strings, kaaskop.",
+                ),
+            },
+            TokenType::Minus => apply_arith_to_nums!(Minus, -),
+            TokenType::Star => apply_arith_to_nums!(Star, *),
+            TokenType::Slash => apply_arith_to_nums!(Slash, /),
+
+            TokenType::Caret => match (left, right) {
+                (Value::Num(num1), Value::Num(num2)) => return Value::Num(num1.powf(num2)),
+                _ => crash(
+                    op.line,
+                    "Caret kan alleen worden gebruikt op nummers, kaaskop.",
                 ),
             },
 
-            // overwrite value from variable name out of hashmap
-            Expr::Assign(name, expr) => {
-                let new_value = self.evaluate_expr(expr);
-                if let Err(msg) = self.env.replace_value(name, &new_value) {
-                    crash(name.line, &msg)
-                }
-                new_value
-            }
-            Expr::Logic(left, op, right) => {
-                match op.kind {
-                    TokenType::And => {
-                        let left = self.evaluate_expr(left).is_true();
-
-                        if let Some(left) = left {
-                            let right = self.evaluate_expr(right).is_true();
-
-                            if let Some(right) = right {
-                                return Value::from_bool(left && right);
-                            } else {
-                                crash(op.line, "'en' kan alleen worden gebruikt op waardigheids waarden, kaaskop.");
-                            }
-                        } else {
-                            crash(
-                                op.line,
-                                "'en' kan alleen worden gebruikt op waardigheids waarden, kaaskop.",
-                            );
-                        }
-                    }
-
-                    TokenType::Or => {
-                        match self.evaluate_expr(left).is_true() {
-                            Some(left) => {
-                                if left {
-                                    return Value::True;
-                                }
-                            }
-                            None => crash(
-                                op.line,
-                                "'of' kan alleen worden gebruikt op waardigheids waarden, kaaskop.",
-                            ),
-                        }
-
-                        match self.evaluate_expr(right).is_true() {
-                            Some(right) => Value::from_bool(right),
-                            None => crash(
-                                op.line,
-                                "'of' kan alleen worden gebruikt op waardigheids waarden, kaaskop.",
-                            ),
-                        }
-                    }
-                    _ => panic!("Unreachable."),
-                }
-            }
+            TokenType::Greater => apply_logic_to_nums!(Greater, >),
+            TokenType::GreaterEqual => apply_logic_to_nums!(GreaterEqual, >=),
+            TokenType::Less => apply_logic_to_nums!(Less, <),
+            TokenType::LessEqual => apply_logic_to_nums!(LessEqaul, <=),
+            TokenType::Equal => apply_logic_to_nums!(Equal, ==),
+            TokenType::EqualEqual => Value::from_bool(Value::is_equal(&left, &right)),
+            TokenType::BangEqual => Value::from_bool(!Value::is_equal(&left, &right)),
+            _ => panic!("Unreachable."),
         }
+    }
+
+    fn evaluate_logic(&mut self, left: &Box<Expr>, op: &Token, right: &Box<Expr>) -> Value {
+        match op.kind {
+            TokenType::And => {
+                let left = self.evaluate_expr(left).is_true();
+
+                if let Some(left) = left {
+                    let right = self.evaluate_expr(right).is_true();
+
+                    if let Some(right) = right {
+                        return Value::from_bool(left && right);
+                    } else {
+                        crash(
+                            op.line,
+                            "'en' kan alleen worden gebruikt op waardigheids waarden, kaaskop.",
+                        );
+                    }
+                } else {
+                    crash(
+                        op.line,
+                        "'en' kan alleen worden gebruikt op waardigheids waarden, kaaskop.",
+                    );
+                }
+            }
+
+            TokenType::Or => {
+                match self.evaluate_expr(left).is_true() {
+                    Some(left) => {
+                        if left {
+                            return Value::True;
+                        }
+                    }
+                    None => crash(
+                        op.line,
+                        "'of' kan alleen worden gebruikt op waardigheids waarden, kaaskop.",
+                    ),
+                }
+
+                match self.evaluate_expr(right).is_true() {
+                    Some(right) => Value::from_bool(right),
+                    None => crash(
+                        op.line,
+                        "'of' kan alleen worden gebruikt op waardigheids waarden, kaaskop.",
+                    ),
+                }
+            }
+            _ => panic!("Unreachable."),
+        }
+    }
+
+    fn evaluate_var(&mut self, token: &Token) -> Value {
+        match self.env.get_value(&token) {
+            Some(value) => value,
+            None => crash(
+                token.line,
+                &format!("{} is een onbekende variabele.", token.lexeme),
+            ),
+        }
+    }
+
+    fn evaluate_assign(&mut self, name: &Token, expr: &Box<Expr>) -> Value {
+        let new_value = self.evaluate_expr(expr);
+        if let Err(msg) = self.env.replace_value(name, &new_value) {
+            crash(name.line, &msg)
+        }
+        new_value
     }
 }
